35c84b9412e1edd92a9697b6b7e002a606ad761a
Robert Krahn <robert.krahn@gmail.com>
2017-09-14 00:12:16 +0000

    fix addWrappers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const defaultConsoleMethods = ["log", "group", "groupEnd", "warn", "assert", "error"];
 
 export function prepareConsole(platformConsole, consoleMethods = defaultConsoleMethods, _window = window) {
+
+  if (platformConsole.wasPrepared) return;
+  platformConsole.wasPrepared = true;
+
   for (var i = 0; i < consoleMethods.length; i++)
     if (!platformConsole[consoleMethods[i]])
       platformConsole[consoleMethods[i]] = emptyFunc;
@@ -35,43 +39,37 @@ export function prepareConsole(platformConsole, consoleMethods = defaultConsoleM
 
   function addWrappers() {
     if (platformConsole.wasWrapped) return;
+    platformConsole.wasWrapped = true;
 
-    var props = [];
-    for (var name in platformConsole) props.push(name);
-
-    var exceptions = ["removeWrappers", "addWrappers", "addConsumer", "removeConsumer"],
+    let exceptions = ["removeWrappers", "addWrappers", "addConsumer", "removeConsumer"],
+        methods = Object.keys(platformConsole)
+          .filter(name => typeof platformConsole[name] === "function"
+                  && !name.startsWith("$")
+                  && !platformConsole.hasOwnProperty("$" + name)
+                  && !exceptions.includes(name)),
         activationState = {};
 
-    for (var i = 0; i < props.length; i++) {
-      var name = props[i];
-      if (
-        name[0] === "$" ||
-        typeof platformConsole[name] !== "function" ||
-        exceptions.indexOf(name) > -1
-      ) continue;
-
-      (function(name) {
-        var func = platformConsole[name];
-        platformConsole["$" + name] = func;
-        platformConsole[name] = function() {
-          if (activationState[name]) return;
-          activationState[name] = true;
-          try {
-            func.apply(platformConsole, arguments);
-            for (var i = 0; i < consumers.length; i++) {
-              var consumerFunc = consumers[i][name];
-              if (typeof consumerFunc === "function")
-                consumerFunc.apply(consumers[i], arguments);
-            }
-            for (var i = 0; i < simpleConsumers.length; i++) {
-              var consumerFunc = simpleConsumers[i][name];
-              if (typeof consumerFunc === "function")
-                consumerFunc.call(simpleConsumers[i], formatTemplateString.apply(null, arguments));
-            }
-          } finally { activationState[name] = false; }
-        }
-      })(name);
-    }
+    methods.forEach(name => {
+      var orig = platformConsole[name];
+      platformConsole["$" + name] = platformConsole[name];
+      platformConsole[name] = function() {
+        if (activationState[name]) return;
+        activationState[name] = true;
+        try {
+          platformConsole["$" + name].apply(platformConsole, arguments);
+          for (var i = 0; i < consumers.length; i++) {
+            var consumerFunc = consumers[i][name];
+            if (typeof consumerFunc === "function")
+              consumerFunc.apply(consumers[i], arguments);
+          }
+          for (var i = 0; i < simpleConsumers.length; i++) {
+            var consumerFunc = simpleConsumers[i][name];
+            if (typeof consumerFunc === "function")
+              consumerFunc.call(simpleConsumers[i], formatTemplateString.apply(null, arguments));
+          }
+        } finally { activationState[name] = false; }
+      }
+    });
   }
 
   function removeWrappers() {
@@ -81,6 +79,7 @@ export function prepareConsole(platformConsole, consoleMethods = defaultConsoleM
       platformConsole[realName] = platformConsole[name];
       delete platformConsole[name];
     }
+    platformConsole.wasWrapped = true;
   }
 }
 
